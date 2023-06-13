c200ef1ed5ad8657df1b8f4c3f68eb35dec81097
J.P. Haynes <joeypaulhaynes@yahoo.com>
2023-06-13 01:58:59 -0500

    Added authentication to admin-facing website

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../services/authentication';
 import { Trip } from '../models/trip';
 
 @Component({
@@ -10,16 +12,23 @@ import { Trip } from '../models/trip';
 export class TripCardComponent implements OnInit {
 
   @Input('trip') trip: any;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthenticationService
   ) { }
 
   ngOnInit() {
   }
+
   private editTrip(trip: Trip): void {
     console.log('Inside TripListingComponent#editTrip');
     localStorage.removeItem("tripCode");
     localStorage.setItem("tripCode", trip.code);
     this.router.navigate(['edit-trip']);
+  }
+
+  public isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
 }
